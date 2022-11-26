38dce1dc785624928c108922652aa96f8d580f91
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-11-26 21:10:27 +0200

    Fetch photos and display in screen

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const imageContainer = document.querySelector('.img--container')
 
 // Unsplash API
 const count = 30;   
@@ -6,13 +7,35 @@ const apiURL = `https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}&c
 
 let photosArray = []
 
-// Get photos from Unsplash API
+function displayImages() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank')
+        
+        // img 
+        const image = document.createElement('img');
+        image.setAttribute("src", photo.urls.regular);
+        if(!photo.description) {
+            image.setAttribute('alt' , "Please go to the website");
+            image.setAttribute('title' , "Please go to the website")
+        } else {
+            image.setAttribute('alt' , photo.description);
+            image.setAttribute('title' , photo.description)
+        }
+        
+        // Add to UI
+        item.appendChild(image);
+        imageContainer.appendChild(item)
+    })
+}
 
+// Get photos from Unsplash API
 async function fetchPhotos() {
     try {
         const response = await fetch(apiURL);
         photosArray = await response.json();
-        console.log(photosArray)
+        displayImages() 
     } catch (error) {
         alert(error)
     }
